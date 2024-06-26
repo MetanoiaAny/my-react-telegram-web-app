ab1cdf582a2ee4a6f52284c5db486c6b132aeefa
AutumnFrosti <fragile173@outlook.com>
2024-06-26 16:01:33 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import RenderRouter from "@/route/index";
-import { AppRoot } from "@telegram-apps/telegram-ui";
-import { bindMiniAppCSSVars, useLaunchParams, useMiniApp, useThemeParams } from "@tma.js/sdk-react";
-import { useEffect } from "react";
+// import { AppRoot } from "@telegram-apps/telegram-ui";
+// import { bindMiniAppCSSVars, useLaunchParams, useMiniApp, useThemeParams } from "@tma.js/sdk-react";
+// import { useEffect } from "react";
 // import { useInitData, useLaunchParams } from "@tma.js/sdk-react";
 import { HashRouter } from "react-router-dom";
 function App() {
-
-  const miniApp = useMiniApp();
-  const themeParams = useThemeParams();
-  const lp = useLaunchParams();
 
   console.log(1);
   
@@ -19,22 +15,18 @@ function App() {
   // useEffect(() => {
   //   console.log(initData);
   //   console.log(initDataRaw);
-  // }, []);
-
-  useEffect(() => {
-    return bindMiniAppCSSVars(miniApp, themeParams);
-  }, [miniApp, themeParams]);
+  // }, [])
   
   return (
     <>
-      <AppRoot
+      {/* <AppRoot
         appearance={miniApp.isDark ? "dark" : "light"}
         platform={["macos", "ios"].includes(lp.platform) ? "ios" : "base"}
-      >
+      > */}
         <HashRouter>
           <RenderRouter />
         </HashRouter>
-      </AppRoot>
+      {/* </AppRoot> */}
     </>
   );
 }
--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,41 +1,41 @@
-import { SDKProvider, useLaunchParams } from '@tma.js/sdk-react';
+import { SDKProvider } from '@tma.js/sdk-react';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { type FC, useEffect, useMemo } from 'react';
+import { type FC, useMemo } from 'react';
 
 import App from "@/App";
-import { ErrorBoundary } from '@/components/ErrorBoundary';
+// import { ErrorBoundary } from '@/components/ErrorBoundary';
 
-const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
-  <div>
-    <p>An unhandled error occurred:</p>
-    <blockquote>
-      <code>
-        {error instanceof Error
-          ? error.message
-          : typeof error === 'string'
-            ? error
-            : JSON.stringify(error)}
-      </code>
-    </blockquote>
-  </div>
-);
+// const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
+//   <div>
+//     <p>An unhandled error occurred:</p>
+//     <blockquote>
+//       <code>
+//         {error instanceof Error
+//           ? error.message
+//           : typeof error === 'string'
+//             ? error
+//             : JSON.stringify(error)}
+//       </code>
+//     </blockquote>
+//   </div>
+// );
 
 const Inner: FC = () => {
-  const debug = useLaunchParams().startParam === 'debug';
+  // const debug = useLaunchParams().startParam === 'debug';
   const manifestUrl = useMemo(() => {
     return new URL('tonconnect-manifest.json', window.location.href).toString();
   }, []);
   
   // Enable debug mode to see all the methods sent and events received.
-  useEffect(() => {
-    if (debug) {
-      import('eruda').then((lib) => lib.default.init());
-    }
-  }, [debug]);
+  // useEffect(() => {
+  //   if (debug) {
+  //     import('eruda').then((lib) => lib.default.init());
+  //   }
+  // }, [debug]);
 
   return (
     <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <SDKProvider acceptCustomStyles debug={debug}>
+      <SDKProvider acceptCustomStyles debug>
         <App/>
       </SDKProvider>
     </TonConnectUIProvider>
@@ -44,8 +44,9 @@ const Inner: FC = () => {
 
 export default function Root() {
   return (
-    <ErrorBoundary fallback={ErrorBoundaryError}>
-      <Inner/>
-    </ErrorBoundary>
+    <Inner/>
+    // <ErrorBoundary fallback={ErrorBoundaryError}>
+    //   <Inner/>
+    // </ErrorBoundary>
   );
 }
